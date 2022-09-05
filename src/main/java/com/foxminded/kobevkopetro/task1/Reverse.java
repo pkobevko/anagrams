b94954735cb5bba3cc50e41e41e6df74ee308a4b
Petro Kobevko <kobevkopi@gmail.com>
2022-09-05 21:22:23 +0200

    Added JUnit5 dependency

--- src/main/java/com/foxminded/kobevkopetro/task1/Reverse.java
+++ src/main/java/com/foxminded/kobevkopetro/task1/Reverse.java
@@ -5,26 +5,26 @@ import java.util.stream.Stream;
 
 public class Reverse {
 
-    private static final String REGEX_ONLY_LETTERS = "[^a-zA-Z]";
-    private static final String SEPARATOR = " ";
+	private static final String REGEX_ONLY_LETTERS = "[^a-zA-Z]";
+	private static final String SEPARATOR = " ";
 
-    public String reversePhrase(String input) {
-        return Stream.of(input.split(SEPARATOR)).map(word -> reverseWord(word)).collect(Collectors.joining(SEPARATOR));
-    }
+	public String reversePhrase(String input) {
+		return Stream.of(input.split(SEPARATOR)).map(word -> reverseWord(word)).collect(Collectors.joining(SEPARATOR));
+	}
 
-    private String reverseWord(String word) {
-        String reversedOnlyLetterWord = new StringBuilder(word).reverse().toString().replaceAll(REGEX_ONLY_LETTERS, "");
-        String reversedWord = "";
+	private String reverseWord(String word) {
+		String reversedOnlyLetterWord = new StringBuilder(word).reverse().toString().replaceAll(REGEX_ONLY_LETTERS, "");
+		String reversedWord = "";
 
-        for (int i = 0; i < reversedOnlyLetterWord.length(); i++) {
-            if (Character.isLetter(word.charAt(i))) {
-                reversedWord += reversedOnlyLetterWord.charAt(i);
-            } else {
-                reversedWord += word.charAt(i);
-                reversedWord += reversedOnlyLetterWord.charAt(i);
-            }
-        }
+		for (int i = 0; i < reversedOnlyLetterWord.length(); i++) {
+			if (Character.isLetter(word.charAt(i))) {
+				reversedWord += reversedOnlyLetterWord.charAt(i);
+			} else {
+				reversedWord += word.charAt(i);
+				reversedWord += reversedOnlyLetterWord.charAt(i);
+			}
+		}
 
-        return reversedWord;
-    }
+		return reversedWord;
+	}
 }
